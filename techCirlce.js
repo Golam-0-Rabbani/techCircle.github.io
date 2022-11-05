2a87a594e817d802d3a60119d382d0ee57b9b71b
Golam Rabbani <103634069+golamrabbani01@users.noreply.github.com>
2022-11-05 09:28:15 +0600

    Add files via upload

---
+++ techCirlce.js
@@ -0,0 +1,9 @@
+var sticks = document.querySelector(".sticks");
+var menus = document.querySelector(".menus");
+
+
+sticks.addEventListener("click", ()=>{
+  
+  menus.classList.toggle("afterMenus");
+  
+})
